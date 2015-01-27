0e3848c084451fef56b8894216a73b97a19d93b1
Maxim Fedorov <familiar.hedgehog@gmail.com>
2015-01-27 10:24:26 +0200

    Omit branch name in prompt for regular directories

--- Shell.scala
+++ Shell.scala
@@ -1,14 +1,19 @@
 package tnm
 
+import scala.util.Try
 import sbt._
 
 object Shell {
   import scala.Console._
   private def decorate(msg: String, dec: String) = s"$dec$msg$RESET"
   val prompt = (s: State) => {
-    "%s [%s]> " format (
-      decorate(Project.extract(s).currentProject.id, BOLD),
-      decorate(("git rev-parse --abbrev-ref HEAD".!!).trim, GREEN)
-    )
+    val project = decorate(Project.extract(s).currentProject.id, BOLD)
+    val branch =
+      Try {
+        val b = "git rev-parse --abbrev-ref HEAD".!!.trim
+        " ["+decorate(b, GREEN)+"]"
+      } getOrElse ""
+
+    s"$project$branch> "
   }
 }
