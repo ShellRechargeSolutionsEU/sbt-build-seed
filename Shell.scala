edb606a264b39772a17545a433e99e7bf1709b5b
Maxim Fedorov <m.fedorov@thenewmotion.com>
2016-03-25 16:37:44 +0100

    Display project name instead of id in prompt

--- Shell.scala
+++ Shell.scala
@@ -7,11 +7,11 @@ object Shell {
   import scala.Console._
   private def decorate(msg: String, dec: String) = s"$dec$msg$RESET"
   val prompt = (s: State) => {
-    val project = decorate(Project.extract(s).currentProject.id, BOLD)
+    val project = decorate(Project.extract(s).get(Keys.name), BOLD)
     val branch =
       Try {
-        val b = "git rev-parse --abbrev-ref HEAD".!!.trim
-        " ["+decorate(b, GREEN)+"]"
+        val branch = "git rev-parse --abbrev-ref HEAD".!!.trim
+        " ["+decorate(branch, GREEN)+"]"
       } getOrElse ""
 
     s"$project$branch> "
